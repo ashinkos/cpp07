d156a223e80335c9c4bdf6f96ba23018ed4f731f
Alaa Aouni <aaouni@e2r6p10.1337.ma>
2023-02-24 15:49:06 +0100

    day07 done

--- ex02/Array.hpp
+++ ex02/Array.hpp
@@ -6,33 +6,63 @@
 /*   By: aaouni <aaouni@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/24 03:16:36 by aaouni            #+#    #+#             */
-/*   Updated: 2023/02/24 03:56:23 by aaouni           ###   ########.fr       */
+/*   Updated: 2023/02/24 15:35:07 by aaouni           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef ARRAY_HPP
 #define ARRAY_HPP
 #include <iostream>
+#include <stdexcept>
 
 template<typename T>
 class Array
 {
 private:
-    T* _type;
+    T* _tab;
     unsigned int _size;
 public:
     Array(){
-        _type = new T[0];
+        _tab = new T[0];
         _size = 0;
     };
+    
     Array(unsigned int n){
-        _type = new T[n];
+        _tab = new T[n];
         _size = n; 
     };
-    ~Array(){
-        
+
+    Array(Array& inst){
+        this->_tab = new T[inst._size];
+        this->_size = inst._size;
+        for(unsigned int i = 0; i < inst._size; i++){
+            this->_tab[i] = inst._tab[i];
+        }
     };
-    Array& operator[](unsigned index)
+    
+    Array& operator=(T inst){
+        delete [] this->_tab;
+        this->_tab = new T[inst._size];
+        this->_size = inst._size;
+        for(unsigned int i = 0; i < inst._size; i++){
+            this->_tab[i] = inst._tab[i];
+        }
+        return *this;
+    };
+    
+    ~Array(){
+        delete [] this->_tab;
+    };
+    
+    T& operator[](int index){
+        if (index >= (int)_size || index < 0)
+            throw std::out_of_range("index is out of bounds");
+        return(this->_tab[index]);
+    };
+
+    unsigned int size() const{
+        return(this->_size);
+    }
 };
 
 
--- ex02/main.cpp
+++ ex02/main.cpp
@@ -5,18 +5,93 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: aaouni <aaouni@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/02/24 03:39:42 by aaouni            #+#    #+#             */
-/*   Updated: 2023/02/24 03:44:58 by aaouni           ###   ########.fr       */
+/*   Created: 2023/02/24 15:03:58 by aaouni            #+#    #+#             */
+/*   Updated: 2023/02/24 15:42:19 by aaouni           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Array.hpp"
-int main(){
-    
-    Array<int> A(5);
 
-    for(int i = 0; i < 5; i++){
-        std::cout << A[i] << ":";
-    }
+// int main(){
+
     
-}
+//     Array<int> a(5);
+//     a[1]  = 10;
+//     Array<int> b(a);
+//     std::cout << "a[1] = " << a[1] << std::endl;
+//     std::cout << a.size() << std::endl;
+//     std::cout << "b[1] = " << b[1] << std::endl;
+
+//     a[1]  = 10000;
+//     std::cout << "a[1] = " << a[1] << std::endl;
+//     std::cout << "b[1] = " << b[1] << std::endl;
+
+    
+//     try{
+//         std::cout << a[10] << std::endl;
+        
+//     }
+//     catch(std::exception& exp){
+//         std::cout << exp.what() << std::endl;
+//     }
+
+//     try{
+//         std::cout << a[-2] << std::endl;
+        
+//     }
+//     catch(std::exception& exp){
+//         std::cout << exp.what() << std::endl;
+//     }
+    
+// }
+
+// #define MAX_VAL 750
+// int main(int, char**)
+// {
+//     Array<int> numbers(MAX_VAL);
+//     int* mirror = new int[MAX_VAL];
+//     srand(time(NULL));
+//     for (int i = 0; i < MAX_VAL; i++)
+//     {
+//         const int value = rand();
+//         numbers[i] = value;
+//         mirror[i] = value;
+//     }
+//     //SCOPE
+//     {
+//         Array<int> tmp = numbers;
+//         Array<int> test(tmp);
+//     }
+
+//     for (int i = 0; i < MAX_VAL; i++)
+//     {
+//         if (mirror[i] != numbers[i])
+//         {
+//             std::cerr << "didn't save the same value!!" << std::endl;
+//             return 1;
+//         }
+//     }
+//     try
+//     {
+//         numbers[-2] = 0;
+//     }
+//     catch(const std::exception& e)
+//     {
+//         std::cerr << e.what() << '\n';
+//     }
+//     try
+//     {
+//         numbers[MAX_VAL] = 0;
+//     }
+//     catch(const std::exception& e)
+//     {
+//         std::cerr << e.what() << '\n';
+//     }
+
+//     for (int i = 0; i < MAX_VAL; i++)
+//     {
+//         numbers[i] = rand();
+//     }
+//     delete [] mirror;//
+//     return 0;
+// }
